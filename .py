9b87bc7d834713da18aa06ca9b38345d4b1a1798
Rignchen <119439839+Rignchen@users.noreply.github.com>
2023-06-29 15:46:03 +0200

    Update .py

--- .py
+++ .py
@@ -3,4 +3,6 @@ from shutil import copy
 
 log = ['acacia_log', 'bamboo_block', 'birch_log', 'cherry_log', 'crimson_log', 'dark_oak_log', 'jungle_log', 'mangrove_log', 'oak_log', 'spruce_log', 'warped_log']
 logs = ['acacia_logs', 'bamboo_blocks', 'birch_logs', 'cherry_logs', 'crimson_logs', 'dark_oak_logs', 'jungle_logs', 'mangrove_logs', 'oak_logs', 'spruce_logs', 'warped_logs']
+plank = ['acacia_plank', 'bamboo_block', 'birch_plank', 'cherry_plank', 'crimson_plank', 'dark_oak_plank', 'jungle_plank', 'mangrove_plank', 'oak_plank', 'spruce_plank', 'warped_plank']
+planks = ['acacia_planks', 'bamboo_blocks', 'birch_planks', 'cherry_planks', 'crimson_planks', 'dark_oak_planks', 'jungle_planks', 'mangrove_planks', 'oak_planks', 'spruce_planks', 'warped_planks']
 wood = ['acacia', 'bamboo', 'birch', 'cherry', 'crimson', 'dark', 'jungle', 'mangrove', 'oak', 'spruce', 'warped']
